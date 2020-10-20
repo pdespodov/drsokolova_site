0e429d49f3493a18be6afd9080fe621b5d7889a9
DESKTOP-70KKGOG\pavel <pavel.despodov@fuelinteractive.com>
2020-10-20 10:21:12 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: '**', component: IndexComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'dr-sokolova-site';
+  title = 'Д-р Искра Соколва - GP';
+
+  public toggleMobileMenu(event) {
+    document.getElementById("site-header-menu-icon").classList.toggle("change");
+    document.querySelector(".site-header").classList.toggle("site-header-is-expanded");
+    document.querySelector(".site-header-menu-content").classList.toggle("site-header-menu-content-is-visible");
+  }
 }
